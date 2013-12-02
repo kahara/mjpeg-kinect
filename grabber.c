baa05965a403bb07aa2c125836527fdff4077c51
Joni Kähärä <joni.kahara@gmail.com>
2013-12-02 22:19:27 +0200

    dummy ir frame, too, in addition to rgb

--- grabber.c
+++ grabber.c
@@ -70,11 +70,16 @@ void * grabber(void * args)
 	printf("grabber producing new frame\n");
 #endif
 	output->serial++;
-#ifdef DEBUG
-	printf("%llu\t%p\t%p\t%d\n", output->serial, output->rgb[output->serial % SETUP_BUFFER_LENGTH_G2P].data, test_frame_rgb, SETUP_IMAGE_SIZE_RAW_RGB);
-#endif
-	output->rgb[output->serial % SETUP_BUFFER_LENGTH_G2P].size = SETUP_IMAGE_SIZE_RAW_RGB;
-	memcpy(output->rgb[output->serial % SETUP_BUFFER_LENGTH_G2P].data, test_frame_rgb, SETUP_IMAGE_SIZE_RAW_RGB);
+	
+	if(SETUP_STREAMS & SETUP_STREAM_RGB) {
+	  output->rgb[output->serial % SETUP_BUFFER_LENGTH_G2P].size = SETUP_IMAGE_SIZE_RAW_RGB;
+	  memcpy(output->rgb[output->serial % SETUP_BUFFER_LENGTH_G2P].data, test_frame_rgb, SETUP_IMAGE_SIZE_RAW_RGB);
+	}
+	
+	if(SETUP_STREAMS & SETUP_STREAM_IR) {
+	  output->ir[output->serial % SETUP_BUFFER_LENGTH_G2P].size = SETUP_IMAGE_SIZE_RAW_IR;
+	  memcpy(output->ir[output->serial % SETUP_BUFFER_LENGTH_G2P].data, test_frame_ir, SETUP_IMAGE_SIZE_RAW_IR);	  
+	}
 	
 	sem_post(&output->full);
 	pthread_mutex_unlock(&output->lock);
