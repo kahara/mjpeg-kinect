a8153a922ccd2abae88508cfcf0b7b31f71c6ce8
Joni Kähärä <joni.kahara@gmail.com>
2013-12-03 21:26:12 +0200

    add Bayer decoding

--- preprocessor.c
+++ preprocessor.c
@@ -5,28 +5,8 @@
 #include "preprocessor.h"
 #include "interthread.h"
 
-// https://github.com/OpenKinect/libfreenect/blob/master/src/cameras.c:convert_bayer_to_rgb()
-void preprocess_rgb(uint8_t * in, uint8_t * out, int width, int height)
-{
-  printf("preprocess_rgb()\n");
-}
-
-// https://github.com/OpenKinect/libfreenect/blob/master/src/cameras.c:convert_packed_to_8bit()
-void preprocess_ir(uint8_t * in, uint8_t * out, int width, int height)
-{
-  int vw = 10, n = width * height;
-  uint32_t buffer = 0;
-  int bitsIn = 0;
-  
-  while (n--) {
-    while (bitsIn < vw) {
-      buffer = (buffer << 8) | *(in++);
-      bitsIn += 8;
-    }
-    bitsIn -= vw;
-    *(out++) = buffer >> (bitsIn + vw - 8);
-  }
-}
+void preprocess_rgb(uint8_t * in, uint8_t * out, int width, int height);
+void preprocess_ir(uint8_t * in, uint8_t * out, int width, int height);
 
 void * preprocessor(void * args)
 {
@@ -90,12 +70,113 @@ void * preprocessor(void * args)
       if(SETUP_STREAMS & SETUP_STREAM_IR) {
 	preprocess_ir(ibuf_ir, obuf_ir, SETUP_IMAGE_WIDTH_IR, SETUP_IMAGE_HEIGHT_IR);
       }
-      
     }
-    
-    
-    
   }
   
   return NULL;
+}
+
+// https://github.com/OpenKinect/libfreenect/blob/master/src/cameras.c:convert_bayer_to_rgb() (XXX licensing)
+void preprocess_rgb(uint8_t * in, uint8_t * out, int width, int height)
+{
+  int x,y;
+  uint8_t *dst = out;
+  uint8_t *prevLine;
+  uint8_t *curLine;
+  uint8_t *nextLine;
+  uint32_t hVals;
+  uint32_t vSums;
+  uint8_t hSum;
+  uint8_t yOdd;
+  
+  curLine  = in;
+  nextLine = curLine + width;
+  for (y = 0; y < height; ++y) {
+    if ((y > 0) && (y < height-1))
+      prevLine = curLine - width;
+    else if (y == 0)
+      prevLine = nextLine;
+    else
+      nextLine = prevLine;
+    
+    hVals  = (*(curLine++) << 8);
+    hVals |= (*curLine << 16);
+    vSums = ((*(prevLine++) + *(nextLine++)) << 7) & 0xFF00;
+    vSums |= ((*prevLine + *nextLine) << 15) & 0xFF0000;
+    yOdd = y & 1;
+    
+    for (x = 0; x < width-1; ++x) {
+      hVals |= *(curLine++);
+      vSums |= (*(prevLine++) + *(nextLine++)) >> 1;
+      hSum = ((uint8_t)(hVals >> 16) + (uint8_t)(hVals)) >> 1;
+      
+      if (yOdd == 0) {
+	if ((x & 1) == 0) {
+	  *(dst++) = hSum;
+	  *(dst++) = hVals >> 8;
+	  *(dst++) = vSums >> 8;
+	} else {
+	  *(dst++) = hVals >> 8;
+	  *(dst++) = (hSum + (uint8_t)(vSums >> 8)) >> 1;
+	  *(dst++) = ((uint8_t)(vSums >> 16) + (uint8_t)(vSums)) >> 1;
+	}
+      } else {
+	if ((x & 1) == 0) {
+	  *(dst++) = ((uint8_t)(vSums >> 16) + (uint8_t)(vSums)) >> 1;
+	  *(dst++) = (hSum + (uint8_t)(vSums >> 8)) >> 1;
+	  *(dst++) = hVals >> 8;
+	} else {
+	  *(dst++) = vSums >> 8;
+	  *(dst++) = hVals >> 8;
+	  *(dst++) = hSum;
+	}
+      }
+      
+      hVals <<= 8;
+      vSums <<= 8;
+    }
+    
+    hVals |= (uint8_t)(hVals >> 16);
+    vSums |= (uint8_t)(vSums >> 16);
+    hSum = (uint8_t)(hVals);
+    
+    if (yOdd == 0) {
+      if ((x & 1) == 0) {
+	*(dst++) = hSum;
+	*(dst++) = hVals >> 8;
+	*(dst++) = vSums >> 8;
+      } else {
+	*(dst++) = hVals >> 8;
+	*(dst++) = (hSum + (uint8_t)(vSums >> 8)) >> 1;
+	*(dst++) = vSums;
+      }
+    } else {
+      if ((x & 1) == 0) {
+	*(dst++) = vSums;
+	*(dst++) = (hSum + (uint8_t)(vSums >> 8)) >> 1;
+	*(dst++) = hVals >> 8;
+      } else {
+	*(dst++) = vSums >> 8;
+	*(dst++) = hVals >> 8;
+	*(dst++) = hSum;
+      }
+    }
+  }
+}
+
+// https://github.com/OpenKinect/libfreenect/blob/master/src/cameras.c:convert_packed_to_8bit() (XXX licensing)
+void preprocess_ir(uint8_t * in, uint8_t * out, int width, int height)
+{
+  int vw = 10, n = width * height;
+  uint32_t buffer = 0;
+  int bitsIn = 0;
+  
+  while (n--) {
+    while (bitsIn < vw) {
+      buffer = (buffer << 8) | *(in++);
+      bitsIn += 8;
+    }
+    bitsIn -= vw;
+    *(out++) = buffer >> (bitsIn + vw - 8);
+  }
 }
