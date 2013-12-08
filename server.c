b7aa8de2f053f16f70d4d5d9cec17b50aa56d30a
Joni Kähärä <joni.kahara@gmail.com>
2013-12-08 19:59:17 +0200

    handle connection in thread

--- server.c
+++ server.c
@@ -11,25 +11,28 @@
 #include "server.h"
 #include "interthread.h"
 
-void * serve(void * args);
+void * serve(void * conn);
 
-struct arg {
-  // xxx ...
-  struct channel * input;
-};
+struct channel * frames;
 
 void * server(void * args)
 {
   struct thread_arg * ta = (struct thread_arg *)args;
-  struct channel * input = ta->input;
   struct timeval tv;
   
-  int sock, connection;
-  struct sockaddr_in addr;
-  pthread_t threads[SETUP_SERVER_THREADS] = { 0 };
+  int sock, connection, optval;
+  struct sockaddr_in addr, addr_conn;
+  socklen_t alen;
+  char ipaddress[16];
+  pthread_t thread;
+  
+  frames = ta->input;
   
   // xxx add some error checking logic here
   sock = socket(AF_INET, SOCK_STREAM, 0);
+  optval = 1;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+  
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = inet_addr(SETUP_SERVER_ADDRESS);
@@ -38,25 +41,23 @@ void * server(void * args)
   listen(sock, 8);
   
   while(1) {
-    connection = accept(sock, (struct sockaddr *)NULL, NULL);
-    printf("CONNECTION\n");
-    close(connection);
+    connection = accept(sock, (struct sockaddr *)&addr_conn, &alen);
+#ifdef DEBUG
+    inet_ntop(AF_INET, &addr_conn.sin_addr.s_addr, ipaddress, alen);
+    printf("connection (%d) from %s:%d\n", connection, ipaddress, addr_conn.sin_port);
+#endif
     
-    //pthread_create(&threads[0], NULL, &serve, input);
-    
-    //tv.tv_sec = 0;
-    //tv.tv_usec = SETUP_POLL_DELAY;
-    //select(0, NULL, NULL, NULL, &tv);
+    pthread_create(&thread, NULL, &serve, (void *)connection);
   }
-  
   
   return NULL;
 }
 
 // serving connections with dedicated threads may not scale too well
-void * serve(void * args)
+void * serve(void * conn)
 {
-  struct channel * ch = (struct channel *)args;
+  int connection = (int)conn; // a form of abuse but it works
   
+  close(connection);
   return NULL;
 }
